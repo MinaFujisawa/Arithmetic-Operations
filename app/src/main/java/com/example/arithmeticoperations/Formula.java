1c93267ec7be698e37a4fa65839248be5c7a5cbd
MinaFujisawa <fujimina37@gmail.com>
2017-06-05 00:02:43 -0700

    create questions

---
+++ app/src/main/java/com/example/arithmeticoperations/Formula.java
@@ -0,0 +1,44 @@
+package com.example.arithmeticoperations;
+
+/**
+ * Created by MinaFujisawa on 2017/06/04.
+ */
+
+public class Formula {
+    int mNum_left;
+    int mNum_right;
+    String mOperation;
+    int mNum_answer;
+
+    public int getmNum_left() {
+        return mNum_left;
+    }
+
+    public void setmNum_left(int mNum_left) {
+        this.mNum_left = mNum_left;
+    }
+
+    public int getmNum_right() {
+        return mNum_right;
+    }
+
+    public void setmNum_right(int mNum_right) {
+        this.mNum_right = mNum_right;
+    }
+
+    public String getmOperation() {
+        return mOperation;
+    }
+
+    public void setmOperation(String mOperation) {
+        this.mOperation = mOperation;
+    }
+
+    public int getmNum_answer() {
+        return mNum_answer;
+    }
+
+    public void setmNum_answer(int mNum_answer) {
+        this.mNum_answer = mNum_answer;
+    }
+}
---
+++ app/src/main/java/com/example/arithmeticoperations/FormulaLab.java
@@ -0,0 +1,85 @@
+package com.example.arithmeticoperations;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by MinaFujisawa on 2017/06/04.
+ */
+
+public class FormulaLab {
+    private static FormulaLab sFormulaLab;
+    private Formula[] mFormulaList;
+    private String[] mOperationTypes = {"addition", "subtraction", "times", "division"};
+    private final static int MAX_QUESTION = 20;
+    Random random = new Random();
+
+    public static FormulaLab get(Context context) {
+        if (sFormulaLab == null) {
+            sFormulaLab = new FormulaLab(context);
+        }
+        return sFormulaLab;
+    }
+
+    public FormulaLab(Context context) {
+        mFormulaList = new Formula[MAX_QUESTION];
+        for (int i = 0; i < MAX_QUESTION; i++) {
+            makeFormula(i);
+        }
+    }
+
+    private void makeFormula(int i) {
+        Formula formula = new Formula();
+        String operation = mOperationTypes[random.nextInt(3)];
+        int left, right;
+
+        switch (operation) {
+            case "times":
+                left = random.nextInt(9);
+                right = random.nextInt(9);
+                formula.setmNum_left(left);
+                formula.setmNum_right(right);
+                formula.setmOperation("times");
+                formula.setmNum_answer(left * right);
+                mFormulaList[i] = formula;
+                break;
+
+            case "addition":
+                left = random.nextInt(30) + 1;
+                right = random.nextInt(9) + 1;
+                formula.setmNum_left(left);
+                formula.setmNum_right(right);
+                formula.setmOperation("plus");
+                formula.setmNum_answer(left + right);
+                mFormulaList[i] = formula;
+
+            case "subtraction":
+                do {
+                    left = random.nextInt(30) + 1;
+                    right = random.nextInt(9) + 1;
+                } while (left < right);
+
+                formula.setmNum_left(left);
+                formula.setmNum_right(right);
+                formula.setmOperation("subtraction");
+                formula.setmNum_answer(left - right);
+                mFormulaList[i] = formula;
+
+            case "division":
+                do {
+                    left = random.nextInt(30) + 1;
+                    right = random.nextInt(9) + 1;
+                } while (left < right);
+
+                formula.setmNum_left(left);
+                formula.setmNum_right(right);
+                formula.setmOperation("division");
+                formula.setmNum_answer(left / right);
+                mFormulaList[i] = formula;
+        }
+    }
+
+}
